d08569749d4244a42ad1b2d1374353b4cc2411ba
unknown <mahdiabbasi602@gmail.com>
2022-06-19 21:12:51 +0430

    library update

--- Contacts.h
+++ Contacts.h
@@ -5,24 +5,78 @@
 #include <iostream>
 #include <fstream>
 
+struct {
+	bool operator()(Contact &c1, Contact &c2)
+	{
+		if ((c1.getFName().compare(c2.getFName())) == 0)
+		{
+			return false;
+		}
+		else if ((c1.getFName().compare(c2.getFName())) == 1)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+} compairWithF;
+
+struct {
+	bool operator()(Contact c1, Contact c2)
+	{
+		if ((c1.getLName().compare(c2.getLName())) == 0)
+		{
+			return false;
+		}
+		else if ((c1.getLName().compare(c2.getLName())) == 1)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+} compairWithL;
+
+struct {
+	bool operator()(Contact c1, Contact c2)
+	{
+		if ((c1.getNumber().compare(c2.getNumber())) == 0)
+		{
+			return false;
+		}
+		else if ((c1.getNumber().compare(c2.getNumber())) == 1)
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+} compairWithN;
+
 class Contacts
 {
 private:
-	// 0 for first name - 1 for last name - 2 for number
 	vector <Contact> conVect;
 	std::string path;
 	bool formatDestroy();
 	void displayContact(Contact &);
-	bool compairWithF(Contact&, Contact&);
-	bool compairWithL(Contact&, Contact&);
-	bool compairWithN(Contact&, Contact&);
-
-
 public:
 	Contacts(std::string);
 	bool readingFile();
-	int find();
+	int find(int);
 	void add();
 	void showAll();
+	void displayAContact(int);
+	void deleteAContact(int);
+	void deleteAll();
+	void edit(int);
+	void findMenu(int);
+	void save();
 };
 
